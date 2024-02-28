14419e945983ca596868c4e766af7748282dfc4b
Edgar Eduardo Vega Ruiz <edgarevegar04@gmail.com>
2024-02-28 08:24:49 -0700

    check db

--- database/config.js
+++ database/config.js
@@ -1,30 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-
-const dbConnection = async() => {
-
-    try {
-
-        await mongoose.connect( process.env.MONGODB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-    
-        console.log('Base de datos online');
-
-    } catch (error) {
-        console.log(error);
-        throw new Error('Error a la hora de iniciar la base de datos');
-    }
-
-
-}
-
-
+    console.log("Base de datos online");
+  } catch (error) {
+    console.log("OCURRIO UN PROBLEMA, SIGUIENTE LINEA ES EL ERROR:");
+    console.log(error);
+    console.log("TERMINO EL PROBLEMA:");
+    throw new Error("Error a la hora de iniciar la base de datos");
+  }
+};
 
 module.exports = {
-    dbConnection
-}
+  dbConnection,
+};
